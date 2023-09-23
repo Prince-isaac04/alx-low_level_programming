ee2bd53e791dfc35b60659a716d7f2171167befb
Prince-isaac04 <ajayioluwaseyi1692002@gmail.com>
2023-09-23 06:52:10 -0700

    new

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,19 +1,15 @@
-section .data
-    hello db 'Hello, Holberton!',0
+extern    printf
 
-section .text
-    global _start
+main:
 
-_start:
-    ; Your code here
-    ; You can use syscalls to write to stdout, for example:
-    ; mov rax, 1         ; syscall number for sys_write
-    ; mov rdi, 1         ; file descriptor (1 = stdout)
-    ; mov rsi, hello    ; pointer to the string to print
-    ; mov rdx, 16        ; length of the string
-    ; syscall
+mov   edi, format
 
-    ; Exit the program
-    mov rax, 60        ; syscall number for sys_exit
-    xor rdi, rdi       ; exit status
-    syscall
+xor   eax, eax
+
+call  printf
+
+mov         eax, 0
+
+ret
+
+format: db `Hello, Holberton\n`, 0
