1855da1934cabd588d2cb80b42b02184d91e3b39
Prince-isaac04 <ajayioluwaseyi1692002@gmail.com>
2023-09-23 07:22:36 -0700

    new

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -6,23 +6,14 @@
 
 
 /**
-
  * add_node - adds a new node at the beginning of a linked list
-
  * @head: double pointer to the list_t list
-
  * @str: new string to add in the node
-
- *
-
  * Return: the address of the new element, or NULL if it fails
-
-*/
+ */
 
 list_t *add_node(list_t **head, const char *str)
-
 {
-
 	list_t *new;
 
 	unsigned int len = 0;
@@ -47,8 +38,5 @@ list_t *add_node(list_t **head, const char *str)
 	new->next = (*head);
 
 	(*head) = new;
-
-
 	return (*head);
-
 }
