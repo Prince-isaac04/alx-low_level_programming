9900cbe3914c1cde805017a053305c5d3e732ab5
Prince-isaac04 <ajayioluwaseyi1692002@gmail.com>
2023-09-27 20:42:57 -0700

    new

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -22,7 +22,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 		new_node->next = NULL;
 	else
 		new_node->next = *head;
-	new_node-> = n;
+	new_node->n = n;
 	*head = new_node;
 	return (new_node);
 }
--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -8,10 +8,9 @@ void free_listint(listint_t *head)
 {
 	listint_t *temp;
 
-	while (head)
+	while ((temp = head) != NULL)
 	{
-		temp = head->next;
-		free(head);
-		head = temp;
+		head = head->next;
+		free(temp);
 	}
 }
