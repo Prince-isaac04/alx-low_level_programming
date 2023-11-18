bfd9c520ddc69df7b1b4e3198a8f3d4ad8d053cd
Prince-isaac04 <ajayioluwaseyi@gmail.com>
2023-11-17 18:42:28 -0800

    new

---
+++ 0x18-dynamic_libraries/README.md
@@ -0,0 +1,4 @@
+Without libraries what have we? We have no past and no future
+mandatory
+Score: 0.0% (Checks completed: 0.0%)
+Create a script that creates a dynamic library called liball.so from all the .c files that are in the current directory.
---
+++ 0x18-dynamic_libraries/functions.c
@@ -0,0 +1,91 @@
+#include "main.h"
+
+
+int _putchar(char c)
+{
+	/*code here*/
+}
+int _islower(int c)
+{
+	        /*code here*/
+}
+
+int _isalpha(int c)
+{
+	        /*code here*/
+}
+int _abs(int n)
+{
+	        /*code here*/
+}
+int _isupper(int c)
+{
+	        /*code here*/
+}
+int _isdigit(int c)
+{
+	        /*code here*/
+}
+
+int _strlen(char *s)
+{
+	        /*code here*/
+}
+void _puts(char *s)
+{
+	        /*code here*/
+}
+char *_strcpy(char *dest, char *src)
+{
+	        /*code here*/
+}
+
+int _atoi(char *s)
+{
+	        /*code here*/
+}
+
+char *_strcat(char *dest, char *src)
+{
+	        /*code here*/
+}
+
+char *_strncat(char *dest, char *src, int n)
+{
+	        /*code here*/
+}
+char *_strncpy(char *dest, char *src, int n)
+{
+	        /*code here*/
+}
+
+int _strcmp(char *s1, char *s2)
+{
+	        /*code here*/
+}
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	        /*code here*/
+}
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	        /*code here*/
+}
+char *_strchr(char *s, char c)
+{
+	        /*code here*/
+}
+unsigned int _strspn(char *s, char *accept)
+{
+	        /*code here*/
+}
+char *_strpbrk(char *s, char *accept)
+{
+	        /*code here*/
+}
+
+char *_strstr(char *haystack, char *needle)
+{
+	        /*code here*/
+}
